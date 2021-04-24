a4ec3385866e734f98b1023444b482c2e2fbae23
tthomas <tibinthomas@outlook.com>
2021-04-24 15:17:13 +0530

    basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,19 @@ module.exports = {
     },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-      extend: {},
-    },
+      extend: {
+        colors: {
+          primary: '#FF6363',
+          secondary: {
+            100: '#E2E2D5',
+            200: '#888883'
+          }
+      },
+      fontFamily:{
+        primary:['Nunito']
+      }
+    }
+  },
     variants: {
       extend: {},
     },
