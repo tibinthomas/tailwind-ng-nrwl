f0e2bc977a5a5c1622b4b12c189aaad78f1d5be5
tthomas <tibinthomas@outlook.com>
2021-04-24 09:41:19 +0530

    nx workspace init

---
+++ apps/one/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tream5-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'one';
+}
